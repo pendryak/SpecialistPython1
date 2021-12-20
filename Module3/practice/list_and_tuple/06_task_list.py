1af518ed71e0815077e5a8ad9bc673a1ad0dec28
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-20 21:38:46 +0300

    Update 06_task_list.py

--- Module3/practice/list_and_tuple/06_task_list.py
+++ Module3/practice/list_and_tuple/06_task_list.py
@@ -5,3 +5,19 @@ first_number = int(input())     # Первое число
 second_number = int(input())    # Второе число
 
 # TODO: your code here
+a = min(first_number,second_number)
+b = max(first_number,second_number)
+l = []
+if a % 3 == 0:
+    l.append(a)
+elif (a + 1) % 3 == 0 and a + 1 <= b:
+    a += 1
+    l.append(a)
+elif a + 2 <= b:
+    a += 2
+    l.append(a)
+a += 3
+while a <= b:
+    l.append(a)
+    a += 3
+print(l)
