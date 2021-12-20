79ca66a91d8c44cd5d6fa00eb95db9eda8e99f08
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-20 16:31:37 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -5,3 +5,11 @@ text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
 
 # TODO: your code here
+count = 0
+i = text.find(' ')
+while i != -1:
+    if i > 4:
+        count += 1
+    text = text[i + 1:]
+    i = text.find(' ')
+print(count)
