ecc33c6afe8ccaccf3e754e1395569268deec794
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 12:09:46 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -4,3 +4,10 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
+l = 0
+res = ''
+for name in names:
+    if len(name) > l:
+        res = name
+        l = len(name)
+print(res)
