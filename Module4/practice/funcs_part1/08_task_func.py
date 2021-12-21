749eb68d3ccb1e030e3d942f2f5ac15cc12c76f1
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 15:24:50 +0300

    Update 08_task_func.py

--- Module4/practice/funcs_part1/08_task_func.py
+++ Module4/practice/funcs_part1/08_task_func.py
@@ -1,1 +1,12 @@
 # Напишите функцию находящую n-ое число Фибоначчи.
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return(1)
+    f_0 = 1
+    f_1 = 1
+    f = 0
+    for i in range(n-2):
+        f = f_0 + f_1
+        f_0 = f_1
+        f_1 = f
+    return(f)
