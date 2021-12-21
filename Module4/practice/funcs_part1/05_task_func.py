8d70c42a143dd9824789b518cfa6ccf6648a4227
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 14:32:03 +0300

    Update 05_task_func.py

--- Module4/practice/funcs_part1/05_task_func.py
+++ Module4/practice/funcs_part1/05_task_func.py
@@ -3,7 +3,35 @@
 # Если по заданным точкам треугольник построить нельзя, выведите соответствующее сообщение.
 # Подсказка: для нахождения площади используйте Теорему Герона
 
-# TODO: your code here
+def dist(x1,y1,x2,y2):
+    return math.sqrt((x1-x2)**2+(y1-y2)**2)
 
+def perimeter(l1,l2,l3):
+    return(l1+l2+l3)
+
+def square(l1,l2,l3):
+    p = perimeter(l1,l2,l3)/2
+    return(math.sqrt(p*(p-l1)*(p-l2)*(p-l3)))
+
+
+xa = float(input("xa = "))
+ya = float(input("ya = "))
+xb = float(input("xb = "))
+yb = float(input("yb = "))
+xc = float(input("xc = "))
+yc = float(input("yc = "))
+
+l1 = dist(xa,ya,xb,yb)
+l2 = dist(xa,ya,xc,yc)
+l3 = dist(xc,yc,xb,yb)
+
+p = perimeter(l1,l2,l3)
+s = square(l1,l2,l3)
+
+if s == 0:
+    print("Точки не образуют треугольник")
+else:
+    print("Периметр:", p)
+    print("Площадь:", s)
 
 # Не забудьте протестировать вашу функцию
