a0ceee3d60f66d8dd3aad3a493d6ff37b8129cb6
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 15:15:24 +0300

    Update 07_task_func.py

--- Module4/practice/funcs_part1/07_task_func.py
+++ Module4/practice/funcs_part1/07_task_func.py
@@ -2,3 +2,13 @@
 # где hh - часы, mm- минуты, ss - секунды.
 # Пример:
 # 29143 секунд → 08:05:43
+
+def format_time(seconds):
+    if seconds >= 60*60*100:
+        return("Слишком много секунд для заданного формата (больше 100 часов)")
+    sec = seconds % 60
+    seconds //= 60
+    min = seconds % 60
+    hour = seconds // 60
+    s = str(hour).rjust(2, '0')+':'+str(min).rjust(2, '0')+':'+str(sec).rjust(2, '0')
+    return(s)
