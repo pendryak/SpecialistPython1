a6282774a02c1e20d32539cb158a46f237db80d4
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 12:04:42 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,15 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+n = int(input("n = "))
+numbers = [random.randint(-100, 100) for _ in range(n)]
+#numbers = [2, -5, 8, 9, -25, 25, 4]
+print(numbers)
+squares = []
+for number in numbers:
+    if number >= 0:
+        square = int(math.sqrt(number))
+        if number == square**2:
+            squares.append(square)
+print(squares)
