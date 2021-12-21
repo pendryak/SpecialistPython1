e5e29d418eece91b780203b4f666a5843d843abd
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-21 13:19:14 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -30,16 +30,41 @@ items = [
         "brand": "puma",
         "price": 1700
     },
+{
+        "name": "Футболка",
+        "brand": "puma",
+        "price": 4800
+    },
 ]
 # Найдите:
 print("Товары на складе представлены брэндами: ")
-
-# TODO: your code here
+brands = set(item['brand'] for item in items)
+print(*brands, sep=', ')
 
 print("На складе больше всего товаров брэнда(ов): ")
 
-# TODO: your code here
+brands_dict = {}
+for brand in brands:
+    brands_dict[brand] = 0
+
+
+for item in items:
+    brands_dict[item['brand']] += 1
+max_brands = max(brands_dict.values())
+
+for key,value in brands_dict.items():
+    if value == max_brands:
+        print(key)
+
 
 print("На складе самый дорогой товар брэнда(ов): ")
-
-# TODO: your code here
+items.sort(key=operator.itemgetter('price'))
+max_price_item = len(items)-1
+max_price = items[-1]['price']
+i = -1
+while True:
+    if items[i]['price'] == max_price:
+        print(items[i]['brand'])
+        i -= 1
+    else:
+        break
