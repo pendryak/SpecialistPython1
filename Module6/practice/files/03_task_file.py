d218e8785e7002821430948ed43f30b6b54079b4
pendryak <46635744+pendryak@users.noreply.github.com>
2021-12-24 12:31:14 +0300

    Update 03_task_file.py

--- Module6/practice/files/03_task_file.py
+++ Module6/practice/files/03_task_file.py
@@ -2,3 +2,18 @@
 
 # Задача: выведите всех сотрудников в файл highly_paid.txt, зарплаты которых превышают 60000
 # Сотружников вывести в формате: Фамилия И.О. ,например: Иванов И.П. (зарплаты в файл не записывать)
+
+
+path = "files/salaries.txt"
+path_out = "files/highly_paid.txt"
+
+f = open(path, "r",encoding='utf-8')
+f_out = open(path_out, "w", encoding='utf-8')
+
+lines = f.readlines()
+lines = lines[1:]
+for line in lines:
+    empl = " ".join(line.split()).split()
+    empl[3] = float(empl[3])
+    if empl[3] > 60000:
+        f_out.write(empl[0]+' '+empl[1][0] + '.'+empl[2][0] + '.\n')
